d3cc8b4175c5b1f138cabc1efd59ac7d721b000b
Miguel Angel Diego <Miguel.Angel.Diego@ext.esa.int>
2025-12-05 15:53:35 +0100

    Added new setting to condition merge automatic or not

--- src/extension.ts
+++ src/extension.ts
@@ -2,16 +2,6 @@ import * as vscode from 'vscode';
 import { BitbucketClient } from './bitbucket';
 
 export async function activate(context: vscode.ExtensionContext) {
-    // const gitExt = vscode.extensions.getExtension('vscode.git')?.exports;
-    // const git = gitExt.getAPI(1);
-
-    // git.onDidRunOperation(async (e: any) => {
-    //     if (e.operation === "Push") {
-    //         vscode.window.showInformationMessage("Push detected. Creating Pull Request...");
-    //         await createPullRequest();
-    //     }
-    // });
-
     const cmd = vscode.commands.registerCommand('madhosoi.bitbucketPR.createPR', async () => {
         await createPullRequest();
     });
@@ -26,6 +16,7 @@ async function createPullRequest() {
         const username = config.get("username") as string;
         const appPassword = config.get("appPassword") as string;
         const defaultBranch = config.get("defaultBranch") as string;
+        const autoMerge = config.get("autoMerge") as boolean;
 
         if (!baseUrl || !username || !appPassword) {
             vscode.window.showErrorMessage("Missing Bitbucket configuration.");
@@ -45,26 +36,27 @@ async function createPullRequest() {
             const project = match[2];
             const repoSlug = match[3];
 
-            //vscode.window.showInformationMessage(`Starting PR# on ${project}/${repoSlug}`)
-
             // Add prompt to select the branch to merge
             const branches = await git.repositories[0].getBranches();
             const selectedBranch = await vscode.window.showQuickPick(
-                branches.map((b: any) => b.name), 
+                branches.map((b: any) => b.name),
                 { placeHolder: 'Select the branch to merge' });
 
             if (!selectedBranch) {
                 return;
             }
 
-            //vscode.window.showInformationMessage(`Creating PR# on ${project}/${repoSlug} for ${selectedBranch}`);
-
             const client = new BitbucketClient(baseUrl, username, appPassword);
             const pr = await client.createPR(project, repoSlug, currentBranch!, selectedBranch);
 
-            await client.mergePR(project, repoSlug, pr.id);
+            if (autoMerge) {
+                await client.mergePR(project, repoSlug, pr.id);
 
-            vscode.window.showInformationMessage(`PR #${pr.id} merged.`);
+                vscode.window.showInformationMessage(`PR #${pr.id} merged.`);
+            }
+            else {
+                vscode.window.showInformationMessage(`PR #${pr.id} created.`);
+            }
         }
     } catch (err: any) {
         vscode.window.showErrorMessage(err.message);
